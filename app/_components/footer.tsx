18f7db1ad325d989ff62f2ccd84faf68e65cae1d
nsamedaisy <nsamedaisy@gmail.com>
2024-05-11 16:27:50 +0100

    potential section

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -7,41 +7,41 @@ import { FaFacebookF, FaLinkedin, FaTiktok, FaTwitter } from 'react-icons/fa';
 
 const Footer: React.FC = () => {
     return (
-        <div className='p-10 lg:mx-20'>
-            <h1 className='text-2xl pb-6'>Get in touch with us</h1>
+        <div className='p-10 py-16 lg:mx-20 bg-black text-white'>
+            <h1 className='text-2xl pb-10 font-bold'>Get in touch with us</h1>
 
-            <div className='flex w-full justify-between'>
-                <div className='space-y-4'>
-                    <HiOutlineMailOpen />
-                    <h2 className='font-bold'>Email</h2>
+            <div className='flex w-full justify-between mb-10'>
+                <div className='space-y-2'>
+                    <HiOutlineMailOpen className='text-4xl' />
+                    <h2 className='font-medium text-xl'>Email</h2>
                     <p className='text-sm'>Send us an email and we'll get back to you.</p>
                     <Link href='/' className='text-sm underline'>rebaseacademy@gmail.com</Link>
                 </div>
 
-                <div className='space-y-4'>
-                    <HiOutlinePhone />
-                    <h2 className='font-bold'>Phone</h2>
+                <div className='space-y-2'>
+                    <HiOutlinePhone className='text-4xl' />
+                    <h2 className='font-medium text-xl'>Phone</h2>
                     <p className='text-sm'>Give us a call and let's have a conversation.</p>
                     <Link href='/' className='text-sm underline'>+237 670 063 739</Link>
                 </div>
 
-                <div className='space-y-4'>
-                    <IoLocationSharp />
-                    <h2 className='font-bold'>Location</h2>
+                <div className='space-y-2'>
+                    <IoLocationSharp className='text-4xl' />
+                    <h2 className='font-medium text-xl'>Location</h2>
                     <p className='text-sm'>Give us a call and let's have a conversation.</p>
                     <Link href='/' className='text-sm underline'>Montee Jouvence - Yaounde</Link>
                 </div>
             </div>
 
-            <div>
-                <div>
-                    <GrInstagram />
-                    <FaTwitter />
-                    <FaFacebookF />
-                    <FaLinkedin />
-                    <FaTiktok />
+            <div className='flex items-center justify-between'>
+                <div className='flex space-x-6 items-center'>
+                    <GrInstagram className='text-2xl' />
+                    <FaTwitter className='text-2xl' />
+                    <FaFacebookF className='text-2xl' />
+                    <FaLinkedin className='text-2xl' />
+                    <FaTiktok className='text-2xl' />
                 </div>
-                <div className="text-base lg:flex mr-32 space-x-10">
+                <div className="text-sm lg:flex mr-32 space-x-10">
                     <Link href="/about-us" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">
                         About Us
                     </Link>
