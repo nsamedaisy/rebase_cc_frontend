7d27c6a57c360a0ad958c2af1ade77815e875410
nsamedaisy <nsamedaisy@gmail.com>
2024-05-11 19:31:09 +0100

    origin section

--- app/_components/origin.tsx
+++ app/_components/origin.tsx
@@ -4,13 +4,17 @@ import hero from '../../public/hero.jpg'
 
 const OriginSec: React.FC = () => {
     return (
-        <div className="flex flex-col items-center">
-            <div className="w-full h-4/5">
+        <div className="flex justify-around lg:mx-20 items-center mb-10">
+            <div className=" w-[48%] h-1/2">
                 <Image
                     src={hero}
                     alt="sectionimage"
                     className="w-full h-full object-cover"
                 />
+            </div>
+            <div className="w-[40%]">
+                <h1 className="text-6xl font-serif mb-10">Origin and Mission</h1>
+                <p className="w-[70%]">Rebase Code Camp was founded in response to the growing demand for digital skills and the need to bridge the gap between traditional education and industry requirements. With a team of experienced instructors and industry professionals, we are dedicated to empowering individuals to thrive in the digital economy.</p>
             </div>
 
         </div>
--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -14,4 +14,4 @@ export default function LandingPage() {
             <Footer />
         </div>
     )
-}
+};
