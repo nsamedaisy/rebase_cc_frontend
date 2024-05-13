8f4250b9fea628212d9dea75808723945c30fb79
nsamedaisy <nsamedaisy@gmail.com>
2024-05-13 13:30:09 +0100

    meet team section

--- app/_components/meet-team.tsx
+++ app/_components/meet-team.tsx
@@ -7,44 +7,46 @@ import aris from '../../public/aris.jpg'
 
 const MeetTeam: React.FC = () => {
     return (
-        <div className="lg:mx-36 mb-14">
+        <div className="lg:mx-36 my-14">
             <h1 className="text-6xl mb-6 text-center font-sans">Meet the Team</h1>
-            <h3 className="mb-10 text-xl text-center">Get to know our team of dedicated staff, experienced instructors and industry professionals who have shared in our goals and vission</h3>
+            <h3 className="mb-16 w-[50%] mx-auto text-xl text-center">Get to know our team of dedicated staff, experienced instructors and industry professionals who have shared in our goals and vission</h3>
             <div className="flex justify-around items-center">
 
                 <button
-                    className=" left-7 transform -translate-y-1/2 px-5 py-5 bg-orange-500 font-bold rounded-full"
+                    className=" left-7 transform -translate-y-1/2 px-5 py-5 bg-orange-500 font-bold rounded-full flex items-center justify-center"
                 >
                     <GrPrevious className="text-white text-2xl" />
                 </button>
 
-                <div className="bg-gray-200 p-10 w-[30%]">
-                    <div className="w-20 h-24 my-8">
+                <div className="bg-gray-200 w-80 border-4 border-orange-500 items-center justify-center">
+                    <div className="w-full mb-4 ">
                         <Image
                             src={aris}
                             alt='dp'
-                            className="w-full h-full object-cover border rounded-t-full border-gray-200"
+                            className="w-full h-full object-cover"
                         />
                     </div>
-                    <h2 className="mb-2 font-semibold text-lg">Mr Ngoran Aristid</h2>
-                    <p className="">Software Engineer, xyz company.</p>
-                    <p>He is the Chief support staff. she oversees the smooth running of all operations, projects and programs at Rebase Academy. </p>
+                    <div className="p-4 text-center">
+                        <h2 className="mb-2 font-semibold text-lg">Mr Ngoran Aristid</h2>
+                        <p>Software Engineer, xyz company.</p>
+                        <p className="w-full">He is the Chief support staff. she oversees the smooth running of all operations, projects and programs at Rebase Academy. </p>
+                    </div>
                 </div>
 
-                <div className="bg-gray-200 p-10 w-[30%]">
+                <div className="bg-gray-200 w-80 border-4 border-orange-500">
 
-                    <div className="w-20 h-24 my-8">
+                    <div className="w-full mb-4 ">
                         <Image
                             src={kimi}
                             alt='dp'
-                            className="w-full h-full object-cover border rounded-t-full border-gray-200 "
+                            className="w-full h-full object-cover"
                         />
                     </div>
-
-                    <h2 className="mb-2 font-semibold text-lg">Miss Kimmie Salah</h2>
-                    <p>web Developer, abc company.</p>
-                    <p>she is the Chief support staff. she oversees the smooth running of all operations, projects and programs at Rebase Academy. </p>
-
+                    <div className="p-4 text-center">
+                        <h2 className="mb-2 font-semibold text-lg">Miss Kimmie Salah</h2>
+                        <p>web Developer, abc company.</p>
+                        <p className="w-full">she is the Chief support staff. she oversees the smooth running of all operations, projects and programs at Rebase Academy. </p>
+                    </div>
                 </div>
 
                 <button
